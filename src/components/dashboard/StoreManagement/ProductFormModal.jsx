af717b37aa786a4bf1b686d58e804616a0ba160d
Anamul Hasan <anamul15-3625@diu.edu.bd>
2026-01-07 09:52:47 +0600

    added

--- src/components/dashboard/StoreManagement/ProductFormModal.jsx
+++ src/components/dashboard/StoreManagement/ProductFormModal.jsx
@@ -332,7 +332,11 @@ export default function ProductFormModal({ productData, onSuccess, loadProductFo
                                 </div>
                             </div>
 
-                            <SizesTable />
+                            <SizesTable
+                                formData={formData}
+                                onSizeFieldChange={handleSizeFieldChange}
+                                disabled={isLoadingProduct}
+                            />
 
                             <div className="flex justify-end gap-3 pt-4 border-t">
                                 <Button
